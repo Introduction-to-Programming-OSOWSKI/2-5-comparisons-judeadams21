f7dd3c5168bd247f5ce5684d30b90643d1a2f574
judeadams21 <815386@mystma.org>
2021-01-25 21:39:06 +0000

    Comparisons Ver. I

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+
+def greaterOrEqual(x, y):
+    if x > y or x == y:
+        return True
+    else:
+        return False
+
+def lessOrEqual(x, y):
+    if x < y or x == y:
+        return True
+    else:
+        return False
